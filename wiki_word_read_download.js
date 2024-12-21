e1a0ce6adad3df9d5ad32c1eb33ca082f3e4e830
preetygupta7 <49299013+preetygupta7@users.noreply.github.com>
2024-12-21 16:39:40 +0530

    Create wiki_word_read_download.js

---
+++ wiki_word_read_download.js
@@ -0,0 +1,61 @@
+// let url= ["https://live.staticflickr.com/65535/53912002615_3509a036cc_c.jpg","https://live.staticflickr.com/65535/53912002615_3509a036cc_c.jpg"]
+// let n=3
+
+// const download=(e)=>{
+
+// }
+// import fs from "fs"
+// import file from "../download/file"
+// import { log } from "console"
+// fs.readFileSync("../download/file.txt",(res)=>{
+
+//   console.log( res,"sssssssss")
+
+// })
+// }
+let n=3
+
+const getUrl=(url)=>{
+  return new Promise((resolve,reject)=>{
+    const http=require('http')
+    const https=require('https')
+
+    let clients=http;
+
+    if(url.toString().indexOf('https')==0){
+      // console.log(url.toString().indexOf('https')===0){
+        clients=https
+        clients.get(url,(resp)=>{
+        let data=''
+        resp.on('data',(chunk)=>{
+          data+=chunk;
+        })
+        resp.on('end',()=>{
+
+          console.log(data," check the data")
+          resolve(data)
+        })
+
+      }).on('error',(err)=>{
+        reject(err)
+      })
+
+     
+
+
+    }
+
+  })
+
+}
+
+for(let i=0;i<n;i++){
+
+(async(url)=>{
+  console.log(await getUrl(url)
+
+
+)
+})('https://en.wikipedia.org/wiki/Concurrency')
+
+}
